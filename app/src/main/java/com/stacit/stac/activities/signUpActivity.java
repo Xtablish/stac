09341843f2356ba9475b6358cd3a9672d94c40a8
Xtablish <FWGweh@student.fullsail.edu>
2023-03-04 01:28:07 -0800

    This is the first commit for this version of the application.

---
+++ app/src/main/java/com/stacit/stac/activities/signUpActivity.java
@@ -0,0 +1,16 @@
+package com.stacit.stac.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.stacit.stac.R;
+
+public class signUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
